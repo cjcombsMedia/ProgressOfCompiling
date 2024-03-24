0b61cb25bd58cc658faff52805eee019ec05f2a9
CJ Combs <cj@eleviamedia.com>
2024-03-23 23:56:27 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        ProgressBar1 = New ProgressBar()
+        Timer1 = New Timer(components)
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Timer2 = New Timer(components)
+        disableMMchk = New CheckBox()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        startBtn = New Button()
+        TextBox2 = New TextBox()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Enabled = False
+        ProgressBar1.Location = New Point(8, 43)
+        ProgressBar1.Margin = New Padding(2)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(543, 20)
+        ProgressBar1.Style = ProgressBarStyle.Marquee
+        ProgressBar1.TabIndex = 0
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 1000
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(8, 5)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(480, 32)
+        Label1.TabIndex = 1
+        Label1.Text = "Azure Account Checks for Storage Accounts"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(8, 58)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(116, 32)
+        Label2.TabIndex = 2
+        Label2.Text = "Progress: "
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(473, 151)
+        Button1.Margin = New Padding(2)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(78, 26)
+        Button1.TabIndex = 3
+        Button1.Text = "Quit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Timer2
+        ' 
+        Timer2.Interval = 1000
+        ' 
+        ' disableMMchk
+        ' 
+        disableMMchk.AutoSize = True
+        disableMMchk.Location = New Point(14, 160)
+        disableMMchk.Margin = New Padding(2)
+        disableMMchk.Name = "disableMMchk"
+        disableMMchk.Size = New Size(145, 19)
+        disableMMchk.TabIndex = 4
+        disableMMchk.Text = "Disable MMDB Checks"
+        disableMMchk.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(99, 134)
+        TextBox1.Margin = New Padding(2)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(56, 23)
+        TextBox1.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(8, 136)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(77, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Amount in #:"
+        ' 
+        ' startBtn
+        ' 
+        startBtn.Location = New Point(247, 151)
+        startBtn.Margin = New Padding(2)
+        startBtn.Name = "startBtn"
+        startBtn.Size = New Size(78, 26)
+        startBtn.TabIndex = 7
+        startBtn.Text = "Start"
+        startBtn.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(99, 111)
+        TextBox2.Margin = New Padding(2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(115, 22)
+        TextBox2.TabIndex = 8
+        TextBox2.Text = "28bduhauh2eugwkgdhu"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(8, 113)
+        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(88, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "Azure Account:"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(560, 185)
+        Controls.Add(ProgressBar1)
+        Controls.Add(TextBox2)
+        Controls.Add(Label4)
+        Controls.Add(startBtn)
+        Controls.Add(Label3)
+        Controls.Add(TextBox1)
+        Controls.Add(disableMMchk)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Margin = New Padding(2)
+        Name = "Form1"
+        Text = "DB Compile and Move Tool"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents disableMMchk As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents startBtn As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,134 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+
+    ' Import the SetCursorPos function from user32.dll
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetCursorPos(ByVal X As Integer, ByVal Y As Integer) As Boolean
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Sub mouse_event(dwFlags As Integer, dx As Integer, dy As Integer, dwData As Integer, dwExtraInfo As Integer)
+    End Sub
+
+    Private Const MOUSEEVENTF_LEFTDOWN As Integer = &H2
+    Private Const MOUSEEVENTF_LEFTUP As Integer = &H4
+
+    Private random As New Random()
+
+    Private Sub MoveMouseTo(ByVal x As Integer, ByVal y As Integer)
+        ' Set the cursor position to the specified coordinates
+        SetCursorPos(x, y)
+    End Sub
+    Private Sub MoveMouseRandomly()
+        ' Generate random X and Y coordinates within the screen bounds
+        Dim screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
+        Dim screenHeight As Integer = Screen.PrimaryScreen.Bounds.Height
+        Dim randomX As Integer = random.Next(screenWidth)
+        Dim randomY As Integer = random.Next(screenHeight)
+
+        ' Move the mouse to the random coordinates
+        MoveMouseTo(randomX, randomY)
+    End Sub
+
+    Private Sub SimulateMouseClick()
+        ' Simulate a left mouse button down event
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        ' Simulate a left mouse button up event
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    End Sub
+
+    Private Const TotalMinutes As Integer = 95 ' Total duration in minutes
+    Private progressIncrement As Double = 100 / (TotalMinutes * 60) ' Increment per second to complete 100% in TotalMinutes minutes
+    Private progressValue As Double = 0 ' Initial progress value
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Update the progress value
+        progressValue += progressIncrement
+
+        ' Update the progress bar
+        If progressValue <= 100 Then
+            ProgressBar1.Value = CInt(Math.Round(progressValue)) ' Round the progress value to an integer
+            Label2.Text = $"Progress: {ProgressBar1.Value}%"
+        Else
+            Timer1.Stop() ' Stop the timer when progress reaches 100%
+            Timer2.Stop()
+            ' Hide or disable the progress bar
+            ProgressBar1.Style = ProgressBarStyle.Continuous
+            ProgressBar1.Value = 100
+            'ProgressBar1.Visible = False ' Or ProgressBar1.Enabled = False
+            ' Optionally, perform any other actions you need
+            Label2.Text = "Complile and DB Cleanup completed!"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ProgressBar1.MarqueeAnimationSpeed = 0
+        TextBox1.Text = ""
+        startBtn.Enabled = False
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+    End Sub
+
+
+    Private Sub UpdateProgressBar(progressValue As Integer)
+        ' Update the progress bar value
+        ProgressBar1.Value = progressValue
+
+        ' Check if progress reached 100%
+        If progressValue >= 100 Then
+            ' Stop the animation
+            Timer1.Enabled = False ' Assuming you are using a Timer to update the progress bar
+            ' Hide or disable the progress bar
+            ProgressBar1.Visible = False ' Or ProgressBar1.Enabled = False
+            ' Optionally, perform any other actions you need
+            Label2.Text = "Complile and DB Cleanup completed!"
+            ProgressBar1.MarqueeAnimationSpeed = 0
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        ' Trigger mouse movement every 30 seconds
+        If disableMMchk.Checked = True Then
+            'Do nothing
+        Else
+            MoveMouseRandomly()
+            SimulateMouseClick()
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        startBtn.Enabled = True
+        ' Check if the entered character is a digit or backspace
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = ChrW(Keys.Back) Then
+            e.Handled = True ' Ignore the key press
+            Return
+        End If
+
+        ' Check if the entered number is within the allowed range (1-99)
+        Dim newText As String = TextBox1.Text & e.KeyChar
+        If Not String.IsNullOrEmpty(newText) Then
+            Dim number As Integer
+            If Integer.TryParse(newText, number) AndAlso (number < 1 OrElse number > 99) Then
+                e.Handled = True ' Ignore the key press
+            End If
+        End If
+    End Sub
+
+    Private Sub startBtn_Click(sender As Object, e As EventArgs) Handles startBtn.Click
+        ProgressBar1.MarqueeAnimationSpeed = 30
+        Timer1.Start() ' Start the timer when the form loads
+        ' Start the timer when the form loads
+        Timer2.Interval = 30000 ' 30 seconds
+        Timer2.Start()
+        startBtn.Enabled = False
+    End Sub
+End Class
